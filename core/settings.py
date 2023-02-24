1404c36a4bc36a1fd2638a07f8519178d3ecc3e2
glen <gulyashshsh@gmail.com>
2023-02-24 19:01:49 +0600

    added env files

--- core/settings.py
+++ core/settings.py
@@ -10,21 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ, os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o$w*3(1^=6hy_-ht0q+s^j*2icfvsb1t++503s04(2gd%g@e6t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
+# ALLOWED_HOSTS = ['brooklyn.kg']
 ALLOWED_HOSTS = ['*']
 
 
@@ -155,4 +160,10 @@ SWAGGER_SETTINGS = {
             'in': 'header'
         }
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
